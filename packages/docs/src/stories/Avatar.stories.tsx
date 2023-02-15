b1a323d2e73fdaa3c851ec57c7b29ba3a78328e7
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-15 12:37:21 -0300

    feat (docs): add Avatar stories

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@jerp-ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/jerp86.png',
+    alt: 'Photo of white male, short, black hair, wearing black t-shirt, focused on face',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
