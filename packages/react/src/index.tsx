1c58a1086c470e697df364897e2f1b2404dd908e
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-14 22:38:32 -0300

    feat (Button): update style and add variants

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,31 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
   fontFamily: '$default',
+  fontWeight: '$bold',
+  color: '$white',
   backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  padding: '$4',
+  borderRadius: '$sm',
+  border: 0,
+
+  variants: {
+    size: {
+      small: {
+        fontSize: '$sm',
+        padding: '$2 $4',
+      },
+
+      big: {
+        fontSize: '$md',
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
+
+export type ButtonProps = ComponentProps<typeof Button>
