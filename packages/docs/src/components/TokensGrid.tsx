f407c11a83752084aa360017ddde4f0d0aa8cb4a
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-15 00:35:53 -0300

    feat (component): create TokensGrid

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,26 @@
+interface TokensGridProps {
+  tokens: Record<string, string>
+  hasRemValue?: boolean
+}
+
+export const TokensGrid = ({ tokens, hasRemValue }: TokensGridProps) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Value</th>
+        {hasRemValue && <th>Pixels</th>}
+      </tr>
+    </thead>
+
+    <tbody>
+      {Object.entries(tokens).map(([key, value]) => (
+        <tr key={key}>
+          <td>{key}</td>
+          <td>{value}</td>
+          {hasRemValue && <td>{Number(value.replace('rem', '')) * 16}px</td>}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
