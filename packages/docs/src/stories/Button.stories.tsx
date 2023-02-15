7ee6ee2c220765fae63e61f331ce8deee0c27df8
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-14 22:40:59 -0300

    feat (stories): update path and add first story

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@jerp-ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Enviar'
+  }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big'
+  }
+}
