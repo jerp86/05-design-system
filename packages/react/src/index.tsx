69f914df3ff858b7f0c63907754f0d45d7259d13
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-14 22:15:09 -0300

    chore: update export Button component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,10 +1,8 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
   borderRadius: '$md',
   padding: '$4',
 })
-
-export const App = () => <Button>Hello World</Button>
