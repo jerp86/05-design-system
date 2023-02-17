554537d1afa5c5f2141222e3ed880deb2f725b63
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-17 11:24:50 -0300

    chore (docs): converting text to English

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -5,7 +5,7 @@ export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: <Text>Testando o elemento Box</Text>,
+    children: <Text>Testing the Box component</Text>,
   },
   argTypes: {
     children: {
